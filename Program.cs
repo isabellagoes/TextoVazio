a1a7b5758e82c7e996b939b2638f8022fa4d7259
isabellagoes <141466024+isabellagoes@users.noreply.github.com>
2024-03-18 19:26:02 +0000

    Tinha esquecido dessa comando, legal :)

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("-------------------- Texto Vazio --------------------\n");
+
+Console.Write("Digite um texto a fim de avaliá-lo como vazio ou não: ");
+string texto = Console.ReadLine()!;
+
+Console.WriteLine("\nFalse: Texto");
+Console.WriteLine("True: Vazio\n");
+
+Console.WriteLine(String.IsNullOrEmpty(texto.Trim()));
